8e7ffbf94f74170f4c62951c5a0c38696d86a1f1
dmlee-mz <dmlee@mz.co.kr>
2024-01-25 13:52:52 +0900

    feat: [ARMSQA-19] #comment ✅ AUTO-CODE 재설정 - 24.01.25 #close #time 1h +review SR @313devops
    추신 : 맨앞에 붙일 수 있는 구분
    fix : Bug 류 처리 시 ( patch version up )
    feat : 신규 기능 류 처리 시 ( minor version up )
    perf : 메이저 기능 류 처리 시 ( major version up )
    
    [ Auto-Code::Java-Service-Tree-Framework(JSTF)::313DEVGRP ]
    [Requirement Manage] http://www.a-rms.net
    [Document] http://www.313.co.kr/confluence
    [IssueTracker] http://www.313.co.kr/jira
    [VersionControl] https://github.com/313devgrp
    [CodeReview] http://www.313.co.kr/fecru
    [CICD-Deploy] http://www.313.co.kr/jenkins
    [BuildManager] http://www.313.co.kr/spinnaker
    [ArtifactManager] http://www.313.co.kr/nexus
    [CodeAnalysis] http://www.313.co.kr/sonar
    [Docker Hub] https://hub.docker.com/u/313devgrp
    
    [Kubernetes] http://www.313.co.kr:31380
    [DockerSwarm] http://www.313.co.kr/portainer/#!/auth
    
    [DB Admin] http://www.313.co.kr/phpmyadmin/
    [S3 Admin] http://www.313.co.kr/minio/login
    [MEM Admin] http://www.313.co.kr/redis/
    [NoSql Index] http://www.313.co.kr/elasticsearch/_cat/indices
    [NoSql Node] http://www.313.co.kr/elasticsearch/_nodes?pretty=true
    [Kibana] http://www.313.co.kr/kibana/app/home#/
    [LogStash] http://www.313.co.kr/logstash/_node/stats?pretty
    [ZipKin] http://www.313.co.kr/zipkin/
    [ElasticHQ] http://www.313.co.kr/elastichq/#!/
    [Grafana] http://www.313.co.kr/grafana
    
    [NAS] http://www.313.co.kr:5000/webman/index.cgi
    [Mail] http://www.313.co.kr/mail/
    [Auth] http://www.313.co.kr/auth/
    [RDP] http://www.313.co.kr/guacamole/#/

---
+++ src/main/java/com/arms/test/controller/TestController.java
@@ -0,0 +1,61 @@
+/*
+ * @author Dongmin.lee
+ * @since 2024-01-25
+ * @version 24.01.25
+ * @see <pre>
+ *  Copyright (C) 2007 by 313 DEV GRP, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by 313 developer group <313@313.co.kr>, December 2010
+ * </pre>
+ */
+package com.arms.test.controller;
+
+
+import com.egovframework.javaservice.treeframework.controller.TreeAbstractController;
+import com.egovframework.javaservice.treeframework.util.Util_TitleChecker;
+import com.egovframework.javaservice.treeframework.validation.group.AddNode;
+import com.egovframework.javaservice.treeframework.util.ParameterParser;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
+import org.springframework.web.servlet.ModelAndView;
+import javax.annotation.PostConstruct;
+
+import com.arms.test.model.TestEntity;
+import com.arms.test.model.TestDTO;
+import com.arms.test.service.Test;
+
+@Slf4j
+@Controller
+@RestController
+@AllArgsConstructor
+@RequestMapping(value = {"/arms/test"})
+public class TestController extends TreeAbstractController<Test, TestDTO, TestEntity> {
+
+    @Autowired
+    @Qualifier("test")
+    private Test test;
+
+    @PostConstruct
+    public void initialize() {
+        setTreeService(test);
+		setTreeEntity(TestEntity.class);
+    }
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+}
---
+++ src/main/java/com/arms/test/service/TestImpl.java
@@ -0,0 +1,39 @@
+/*
+ * @author Dongmin.lee
+ * @since 2024-01-25
+ * @version 24.01.25
+ * @see <pre>
+ *  Copyright (C) 2007 by 313 DEV GRP, Inc - All Rights Reserved
+ *  Unauthorized copying of this file, via any medium is strictly prohibited
+ *  Proprietary and confidential
+ *  Written by 313 developer group <313@313.co.kr>, December 2010
+ * </pre>
+ */
+package com.arms.test.service;
+
+import com.egovframework.javaservice.treeframework.service.TreeServiceImpl;
+import org.hibernate.criterion.Criterion;
+import org.hibernate.criterion.Order;
+import org.hibernate.criterion.Restrictions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+import java.util.Comparator;
+import java.util.List;
+import java.util.NoSuchElementException;
+import lombok.AllArgsConstructor;
+import com.arms.test.dao.TestRepository;
+
+
+@Slf4j
+@AllArgsConstructor
+@Service("test")
+public class TestImpl extends TreeServiceImpl implements Test{
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	private final TestRepository testRepository;
+
+}
